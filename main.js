a6e08140395ded46a031a46a463c538f68c24284
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-27 14:46:15 +0100

    1 milestone done,carousel works,now focus on spots

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 var app = new Vue({
  el: '#root',
  data:{
-    
+   imgList: [
+     'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+     'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+     'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+     'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+   ],
+   index : 0,
  },
  methods:{
-
-
+    next: function(){
+      this.index++
+      if (this.index === this.imgList.length) {
+        this.index = 0
+      }
+      console.log(this.index);
+      return this.index
+    },
+    prev: function(){
+      this.index--
+      if (this.index == -1 ) {
+        this.index = this.imgList.length - 1;
+      }
+      console.log(this.index);
+      return this.index
+    }
  },
 });
 
